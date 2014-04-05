b262e74516dc0c046c8e39293b746668f4638995
Will Beason <willbeason@gmail.com>
2014-04-04 23:47:45 -0500

    Continued working with ship randomizer

--- Battleship.py
+++ Battleship.py
@@ -5,25 +5,46 @@ from GreedyAI import *
 from Display import *
 from Options import *
 
+def CalcPos(grid,shipSD):
+    spotscores = Flatten(CalcOps(grid,shipSD))
+    tot = sum(spotscores)
+    posscore = random.randint(1,tot)
+
+    position = -1
+    possum = 0
+    while possum<posscore:
+        position += 1
+        possum += spotscores[position]
+
+    xpos = int(position % 10)
+    ypos = int(position / 10)
+    print(xpos,ypos)
+    return(xpos,ypos)
+
 def PositionShips(grid,ships):
     for ship in ships:
         print(ship)
         shipSD = [Sdx(i,[ship]) for i in range(1,11)]
         PrintBoard(CalcOps(grid,shipSD))
+        shipcenter = CalcPos(grid,shipSD)
+        print(shipcenter)
+        grid[shipcenter[1]][shipcenter[0]] = ship
 
 def mainmenu():
     menuops = open('menuoptions.txt','r').read()
     print(menuops)
     option = -1
-    while option not in range(4):
+    while option not in range(5):
         option = int(input("What would you like to do? "))
     return(option)
+
+
+curgrid = [['-']*10]*10
 
 def main():
     mainchoice = 0
  
     shipposgrid = [['-']*10]*10
-    curgrid = [['-']*10]*10
     
     print(open('intro.txt','r').read())
 
